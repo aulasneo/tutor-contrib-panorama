1cc01a12e9324a6cc3150e336f2ea064db20380c
Andrés González <andres@aulasneo.com>
2025-04-23 08:43:56 -0300

    build: Version 19.0.0

--- tutorpanorama/__about__.py
+++ tutorpanorama/__about__.py
@@ -1,4 +1,4 @@
 """
 Package version.
 """
-__version__ = "18.2.1"
+__version__ = "19.0.0"
--- tutorpanorama/plugin.py
+++ tutorpanorama/plugin.py
@@ -21,7 +21,7 @@ PANORAMA_OPENEDX_BACKEND_VERSION = '16.0.12'
 PANORAMA_MFE_REPO = "https://github.com/aulasneo/frontend-app-panorama.git"
 
 # Tag at https://github.com/aulasneo/frontend-app-panorama.git
-PANORAMA_MFE_VERSION = 'open-release/redwood.20240729'
+PANORAMA_MFE_VERSION = 'open-release/sumac/v20250423'
 
 # Tag at https://github.com/aulasneo/panorama-elt.git
 PANORAMA_ELT_VERSION = 'v0.3.2'
